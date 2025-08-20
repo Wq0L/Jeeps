af6b82cc4d741587c3fd64907e4edcc7fb218cd5
Wq0L <cemalcanbekmezci@gmail.com>
2025-08-20 13:19:24 +0300

    V0.2.0

--- Assets/_GameAssets/Scripts/Managers/SkillManager.cs
+++ Assets/_GameAssets/Scripts/Managers/SkillManager.cs
@@ -13,9 +13,53 @@ public class SkillManager : NetworkBehaviour
     {
         _skillDictionary = new Dictionary<SkillType, MysteryBoxSkillsSO>();
 
-        foreach(MysteryBoxSkillsSO skill in _mysteryBoxSkills)
+        foreach (MysteryBoxSkillsSO skill in _mysteryBoxSkills)
         {
-           _skillDictionary[skill.SkillType] = skill;
+            _skillDictionary[skill.SkillType] = skill;
+        }
+    }
+
+    private void Spawn(SkillTransformDataSerializable skillTransformDataSerializable,
+    ulong spawnerClientId, MysteryBoxSkillsSO skillData)
+    {
+        if (IsServer)
+        {
+            Transform skillInstance = Instantiate(skillData.SkillData.SkillPrefab);
+            skillInstance.SetPositionAndRotation(skillTransformDataSerializable.Position, skillTransformDataSerializable.Rotation);
+            var networkObject = skillInstance.GetComponent<NetworkObject>();
+            networkObject.SpawnWithOwnership(spawnerClientId);
+
+            if (NetworkManager.Singleton.ConnectedClients.TryGetValue(spawnerClientId, out var client))
+            {
+                if (skillData.SkillType != SkillType.Rocket)
+                {
+                    networkObject.TrySetParent(client.PlayerObject);
+                }
+                else
+                {
+                    //roket özel
+                }
+
+                if (skillData.SkillData.ShouldBeAttacdhedToParent)
+                {
+                    networkObject.transform.localPosition = Vector3.zero;
+                }
+
+                PositionDataSerializable positionDataSerializable = new PositionDataSerializable(
+                    skillInstance.transform.localPosition + skillData.SkillData.SkillOffset);
+                UpdateSkillPositionRpc(networkObject.NetworkObjectId, positionDataSerializable);
+
+                //devam edecek
+            }
+        }
+
+    }
+    [Rpc(SendTo.ClientsAndHost)]
+    private void UpdateSkillPositionRpc(ulong objectId, PositionDataSerializable positionDataSerializable)
+    {
+        if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(objectId, out NetworkObject networkObject))
+        {
+            networkObject.transform.localPosition = positionDataSerializable.Position;
         }
     }
 }   
